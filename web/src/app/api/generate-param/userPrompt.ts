e50ce8b5e1723b5c2284d33b0ffd0411e2b5c16b
mehekn <mehek.niwas@gmail.com>
2025-02-08 15:28:35 -0500

    locally working api call and prompts

---
+++ web/src/app/api/generate-param/userPrompt.ts
@@ -0,0 +1,56 @@
+export const userPrompt = `instagram:
+{"Profile":"{\"username\": \"psychoticalan\", \"profile_pic\": \"https://scontent-iad3-2.cdninstagram.com/v/t51.2885-19/81427183_461664514462476_3312018366218108928_n.jpg?stp=dst-jpg_e0_s150x150_tt6&_nc_ht=scontent-iad3-2.cdninstagram.com&_nc_cat=100&_nc_ohc=MnATauB0cvwQ7kNvgFAQgvT&_nc_gid=11b0f426647d46e8919371b1499d5790&edm=AJlpnE4BAAAA&ccb=7-5&oh=00_AYBhdh4J_xpUKTW05AkV_lqVzWNqMWiR1XErGs4H_fvFTg&oe=67AD852F&_nc_sid=125e1d\", \"bio\": \"\\ud83c\\uddfa\\ud83c\\uddf8\\ud83e\\udd1d\\ud83c\\uddef\\ud83c\\uddf5\\ud83e\\udd73\", \"posts\": [{\"url\": \"https://scontent-iad3-1.cdninstagram.com/v/t51.29350-15/445359479_417283184402476_2496981196420097492_n.heic?stp=dst-jpg_e35_tt6&_nc_ht=scontent-iad3-1.cdninstagram.com&_nc_cat=104&_nc_ohc=0Ne_lF8BzHEQ7kNvgE5ImNC&_nc_gid=85e240c9fff94f919c8669feff93d4d2&edm=APU89FABAAAA&ccb=7-5&oh=00_AYCEg4UsbOdHf3ryP2Rps2n0dmcGO6pUfxXAE5T5eC-Cxw&oe=67AD730C&_nc_sid=bc0c2c\", \"caption\": \"I \\u2665\\ufe0f black\"}, {\"url\": \"https://scontent-iad3-1.cdninstagram.com/v/t51.29350-15/360208760_312152674474046_3280610036577715233_n.heic?stp=dst-jpg_e35_tt6&_nc_ht=scontent-iad3-1.cdninstagram.com&_nc_cat=102&_nc_ohc=mH95UjtKdZ0Q7kNvgFG6Mf9&_nc_gid=85e240c9fff94f919c8669feff93d4d2&edm=APU89FABAAAA&ccb=7-5&oh=00_AYBMgOzOec2bdYQQYSTJXa-pqlCpqsy3kkn28hK0fsDRcg&oe=67AD8365&_nc_sid=bc0c2c\", \"caption\": \"Frants trip \\ud83c\\uddeb\\ud83c\\uddf7\\ud83c\\uddeb\\ud83c\\uddf7\\ud83d\\ude0f\"}, {\"url\": \"https://scontent-iad3-1.cdninstagram.com/v/t51.29350-15/307219204_5760739370645122_6513596977800998762_n.webp?stp=dst-jpg_e35_tt6&_nc_ht=scontent-iad3-1.cdninstagram.com&_nc_cat=110&_nc_ohc=De6UkvUCpq0Q7kNvgHyLMec&_nc_gid=85e240c9fff94f919c8669feff93d4d2&edm=APU89FABAAAA&ccb=7-5&oh=00_AYANEcZ9_3SCPbGO0KGJKDtMN9lcgxQjSbJYygel8jeOYQ&oe=67AD539A&_nc_sid=bc0c2c\", \"caption\": \"19:43 5k! swipe for sexy picture of me running\"}, {\"url\": \"https://scontent-iad3-2.cdninstagram.com/v/t51.29350-15/270214453_971463643803267_853755778602898556_n.webp?stp=dst-jpg_e35_tt6&_nc_ht=scontent-iad3-2.cdninstagram.com&_nc_cat=103&_nc_ohc=g1rmlMuL_DgQ7kNvgHMVNbc&_nc_gid=85e240c9fff94f919c8669feff93d4d2&edm=APU89FABAAAA&ccb=7-5&oh=00_AYBWo-ThdslBGxAM3OGce8_-IM-96NLr8w3DcNConLSoRA&oe=67AD5345&_nc_sid=bc0c2c\", \"caption\": \"Allah > jose\"}]}"}
+resume:
+Mehek Niwas
+ mehek.niwas@gmail.com | linkedin.com/in/mehek-niwas | github.com/mehek-niwas
+ Education
+ Rutgers University, Honors College
+ Computer Science and Mathematics, B.S.
+ • Dean’s Scholarship Recipient, Wabtec Scholarship Foundation Recipient
+ New Brunswick, NJ
+ Expected Graduation: May 2027
+ • Relevant Courses: Intro to Data Science, Data Structures, Numerical Analysis, Linear Algebra
+ Florida Institute of Technology
+ Dual Enrollment
+ Melbourne, FL
+ • Relevant Courses: Deep Learning, Machine Learning Principles, Intro to PDEs, Discrete Math, Calc 3
+ Experience
+ AI/ML Development Intern
+ Data Glacier
+ Sep. 2024- Dec. 2024
+ Remote
+ • Optimized benchmark CNN & RNN models by 20% to an average of 70% accuracy in medical dataset classification
+ • Utilized HPC Cluster for GPU Acceleration in model training, through Linux OS
+ • Installed packages and managed virtual environments within HPC Cluster using Miniconda3
+ Projects
+ Fashion MNIST Classification With PCA & QDA Optimization
+ • Improved model accuracy from 66% to 81% by combining unsupervised PCA and supervised QDA
+ • Implemented inverse transform for feature visualization on Sparse PCA, Incremental PCA, and Batch PCA
+ • Analyzed class F1 scores and overfitting during parameter tuning to determine model pipeline
+ • ML Techniques: Feature Visualization, Principal Component Analysis, Quadratic Discriminant Analysis
+ Recurrent Neural Networks Optimization for EEG-Based Classification of Brain Activity
+ • Conducted parameter tuning on RNN cell type, hidden size, and dropout rate to optimize model by 35%
+ • Achieved an average accuracy of 71% on over 11,500 EEG recordings from 500+ patients
+ • Analyzed class F1 scores and overfitting during parameter tuning to determine model pipeline
+ • DL Techniques: GPU Acceleration, Recurrent Neural Networks, LSTM, GRU, Parameter Tuning
+ Scraply Best Developer Tool @ HackPrinceton
+ • Developed an interactive web application for visually creating and training custom neural networks (NNs)
+ • Built a dynamic API that constructs and trains NNs using PyTorch, tailored to user specifications
+ • Integrated model training on Flask with logic to provide actionable feedback on model improvement
+ • Deployed on AWS EC2 with Terraform, establishing a scalable infrastructure for seamless model training
+ • Tools: CUDA, PyTorch, Flask, AWS, Terraform, Git
+ SkinSense Special Mention @ Rutgers Health Hack | Team Lead
+ • Led a team of 9 to develop a website to detect monkeypox and other possible skin diseases from uploaded photos
+ • Trained multiple YOLO object classification models for monkeypox skin disease detection with 1,000+ images
+ • Integrated YOLO models using Flask, and deployed with AWS for real-time procressing and predictions
+ • Tools: Ultralytics, Flask, AWS, Git
+ Technical Skills
+ Languages: Python, C/C++, Java, JavaScript, MATLAB, R, LATEX
+ Tools: Linux, Git, Anaconda, Google Cloud Platform, MongoDB, Flask, AWS
+ Libraries: PyTorch, Scikit-Learn, Keras, Gensim, Ultralytics, Pandas, NumPy, Matplotlib
+ High-Performance Computing: SLURM workload manager, GPU Acceleration with CUDA
+ Awards
+ Best Developer Tool @ HackPrinceton– project recognized for creativity & enhancement of developer experience
+ Special Mention @ Rutgers Health Hack– $250 Prize– for innovation and integration of AI in SkinSense project
+ State Bid, 2nd Place @ HOSA Regional (Pharmacology)– in drug classification, interaction, and dosage
+ Regional Finalist, 2nd Place @ North Florida Brain Bee– in neuroanatomy, neural development, and disorders`
