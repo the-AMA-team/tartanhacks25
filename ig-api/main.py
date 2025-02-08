f5d4fb7bca525463ea95c84ec8043b1d81d7de40
Oajoeawfjo-Jioa <alawnjr@gmail.com>
2025-02-07 22:18:12 -0500

    add something

--- ig-api/main.py
+++ ig-api/main.py
@@ -40,7 +40,7 @@ def getPosts(username, password):
 
     posts = []
 
-    for i in range(len(medias)):
+    for i in range(len(medias)) if len(medias <= 20) else 20:
         if medias[i].thumbnail_url != None:
             posts.append(Post(medias[i].thumbnail_url, medias[i].caption_text))
         else:
