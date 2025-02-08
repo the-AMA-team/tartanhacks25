e6bb6d84ac16969fec95ff2f03ee7712bff67d25
Oajoeawfjo-Jioa <alawnjr@gmail.com>
2025-02-07 22:16:47 -0500

    fix multiple images

--- ig-api/main.py
+++ ig-api/main.py
@@ -8,7 +8,7 @@ class Profile:
         self.posts = posts
     
     def __str__(self):
-        return "" + self.username + " " + str(self.profile_pic) + " " + self.bio + " " + str(self.posts)
+        return "" + self.username + " " + str(self.profile_pic) + " " + self.bio
         
 class Post:
     def __init__(self, url, caption):
@@ -18,42 +18,51 @@ class Post:
     def __str__(self):
         return "" + str(self.url) + " " + self.caption
 
+def getPosts(username, password):
 
-cl = Client()
-cl.delay_range = [1, 3]
+    cl = Client()
+    cl.delay_range = [1, 3]
 
-try:
-    cl.login("saucy.boii.tybalt", "slayGirl")
-except:
-    print("Failed to login")
+    try:
+        cl.login(username, password)
+    except:
+        print("Failed to login")
+
+    user_id = cl.user_id_from_username(username)
+    print("User ID", user_id)
+
+    medias = cl.user_medias(user_id, 20)
+    # print("Medias", medias)
+    dict = cl.account_info().model_dump() # not sure if it is actually a dictionary
+    keys_list = list(dict.keys())
+
+    # print(keys_list)
+
+    posts = []
+
+    for i in range(len(medias)):
+        if medias[i].thumbnail_url != None:
+            posts.append(Post(medias[i].thumbnail_url, medias[i].caption_text))
+        else:
+            #lmao
+            posts.append(Post(medias[i].image_versions2["candidates"][0]["url"], medias[i].caption_text))
 
 
 
-user_id = cl.user_id_from_username("saucy.boii.tybalt")
-print("User ID", user_id)
+    profile = Profile(dict["username"], dict["profile_pic_url"], dict["biography"], posts)
 
-medias = cl.user_medias(user_id, 20)
-print(medias)
-dict = cl.account_info().model_dump() # not sure if it is actually a dictionary
-keys_list = list(dict.keys())
+    print("medias 0", medias[0])
 
-print(keys_list)
-1
-posts = []
+    print("Profile: ")
+    print(profile)
+    print("Posts: ")
+    for i in range(len(profile.posts)):
+        print(profile.posts[i])
 
-for i in range(len(medias)):
-    posts.append(Post(medias[i].thumbnail_url, medias[i].caption_text))
+    # for i in range(len(keys_list)):
+    #     print(keys_list[i])
 
+    #print(cl.account_info().model_dump())
+    return profile
 
-profile = Profile(dict["username"], dict["profile_pic_url"], dict["biography"], posts)
-
-print("Profile: ")
-print(profile)
-print("Posts: ")
-for i in range(len(profile.posts)):
-    print(profile.posts[i])
-
-# for i in range(len(keys_list)):
-#     print(keys_list[i])
-
-#print(cl.account_info().model_dump())
+getPosts("psychoticalan", "lueal040106Nn#")
