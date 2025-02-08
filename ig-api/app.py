798d4deea6c51f484865d52346910576c6221e27
Oajoeawfjo-Jioa <alawnjr@gmail.com>
2025-02-08 00:38:48 -0500

    flask server

--- ig-api/app.py
+++ ig-api/app.py
@@ -1,10 +1,14 @@
 from flask import Flask
 from flask_cors import CORS
+import posts
 
 app = Flask(__name__)
 CORS(app)
 
+@app.route("/test")
+def stuff():
+    return "<p> hi <p>"
 
 @app.route("/u/<username>/p/<password>")
 def get_data(username, password):
-    return {"username": username, "password": password}
+    return posts.getPosts(username, password)
