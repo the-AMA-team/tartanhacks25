b2bfa71cfca490dca908c255da895b68b28e9358
mehekn <mehek.niwas@gmail.com>
2025-02-07 19:05:25 -0500

    switched account

--- ig-api/main.py
+++ ig-api/main.py
@@ -2,10 +2,14 @@ from instagrapi import Client
 
 cl = Client()
 cl.delay_range = [1, 3]
-cl.login("itsz_mehek", "slayGirl")
+cl.login("saucy.boii.tybalt", "slayGirl")
 
-user_id = cl.user_id_from_username("itsz_mehek")
+user_id = cl.user_id_from_username("saucy.boii.tybalt")
 print(user_id)
 medias = cl.user_medias(user_id, 20)
 print(medias)
-print(cl.account_info().model_dump())
+dict = cl.account_info().model_dump() # not sure if it is actually a dictionary
+keys_list = list(dict.keys())
+for i in range(len(keys_list)):
+    print(keys_list[i])
+#print(cl.account_info().model_dump())
